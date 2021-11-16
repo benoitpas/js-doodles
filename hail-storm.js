2899d83e8467ecd10bab1501ed2915a581cb25a5
Benoit Pasquereau <bpasquereau@gmail.com>
2021-11-16 08:18:37 +0000

    Add hailstone class

--- hail-storm.js
+++ hail-storm.js
@@ -4,9 +4,34 @@ const ctx = canvas.getContext('2d');
 const width = canvas.width = document.getElementById("main").clientWidth;
 const height = canvas.height = window.innerHeight;
 
-ctx.moveTo(0, 0);
+function random(min, max) {
+    const num = Math.floor(Math.random() * (max - min + 1)) + min;
+    return num;
+}
+
+class Hailstone {
+    constructor(x, y, color, size) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.size = size;
+    }
+    draw() {
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+        ctx.fill();
+    }
+}
+  
+ctx.moveTo(0, height);
 ctx.lineTo(width, height);
 ctx.stroke();
 
-function reset() {
+function newHailstone() {
+    let size = random(10,20)
+    let x = random(size, width-size)
+    let y = random(size, height-size)
+    let hs = new Hailstone(x,y,"#0", size)
+    hs.draw()
 }
